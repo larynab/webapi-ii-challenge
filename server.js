c25d08e5720cf8be9e3d24f29f672b7d7dfba6f3
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-03-19 13:01:43 -0700

    server-setup-and-now-implementing-posts-router

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const postsRouter = require('./posts/posts-router.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Lambda Postts API</h>
+    <p>Welcome to the Lambda Posts API</p>
+  `);
+});
+
+server.use('/api/posts', postsRouter);
+
+module.exports = server;
