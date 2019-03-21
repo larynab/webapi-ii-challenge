4a4b0f2c5f8f2c8971a1b162c160dbdc97e4d089
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-03-21 13:22:37 -0700

    add

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const postsRouter = require('./posts/posts-router.js');
+const postsRouter = require('../posts/posts-router.js');
 
 const server = express();
 
